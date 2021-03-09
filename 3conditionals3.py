e85b7c4b41ffec5700519b4c720858f1964e0416
nickr <nicholaspriegel@gmail.com>
2021-03-09 19:48:51 +0100

    changes

--- 3conditionals3.py
+++ 3conditionals3.py
@@ -1,37 +1,13 @@
-#don't use arepl with this
-
-def x():
-    try:
-        score = float(input('Enter score between 0 and 100: '))
-        if score > 100 or score <= 0:
-            print("Error")
-        elif score >= 97:
-            grade = 'A+'
-        elif score >= 94:
-            grade = 'A'
-        elif score >= 90:
-            grade = 'A-'
-        elif score >= 87:
-            grade = 'B+'
-        elif score >= 84:
-            grade = 'B'
-        elif score >= 80:
-            grade = 'B-'
-        elif score >= 77:
-            grade = 'C+'
-        elif score >= 74:
-            grade = 'C'
-        elif score >= 70:
-            grade = 'C-'
-        elif score >= 67:
-            grade = 'D+'
-        elif score >= 64:
-            grade = 'D'
-        elif score >= 60:
-            grade = 'D-'
-        else:
-            grade = 'F'
-        print('Your grade is: '+grade)
-    except:
-        x()
-x()
+score = float(input('Enter score between 0 and 1: '))
+if score >= 1 or score <= 0:
+    print("Error")
+elif score >= .9:
+    print('A')
+elif score >= .8:
+    print('B')
+elif score >= .7:
+    print('C')
+elif score >= .6:
+    print('D')
+else:
+    print('F')
