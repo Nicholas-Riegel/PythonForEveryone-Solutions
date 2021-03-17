65d1dfd9df36874fff941b00a5fe028d2912de67
nickr <nicholaspriegel@gmail.com>
2021-03-17 22:46:08 +0100

    .

---
+++ 11regex2.py
@@ -0,0 +1,24 @@
+import re
+
+name = input('Enter file name: ')
+if len(name) == 0:
+    name = 'mbox-short.txt'
+
+handle = open(name)
+
+list0 = list()
+list1 = list()
+
+for x in handle:
+    y = re.findall('^New Revision: ([0-9.]+)', x)
+    if len(y) < 1: continue
+    list0 += y
+
+for z in list0:
+    list1.append(float(z))
+
+print(sum(list1) / len(list1))
+
+# gets right number for mbox-short.txt
+# gets 38549... for mbox.txt
+# book has 38444... for mbox.txt 
